92598a07c17fffaedfc24924831975e94315078f
zjinsong <65064518+zjinsong@users.noreply.github.com>
2020-10-03 12:07:58 +0800

    Update main.tf

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,28 +1,6 @@
-terraform {
-  required_version = ">= 0.12.0"
-}
-
 provider "aws" {
-  version = ">= 2.28.1"
-  region  = var.region
+ region = var.aws_region
 }
-
-provider "random" {
-  version = "~> 2.1"
-}
-
-provider "local" {
-  version = "~> 1.2"
-}
-
-provider "null" {
-  version = "~> 2.1"
-}
-
-provider "template" {
-  version = "~> 2.1"
-}
-
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
